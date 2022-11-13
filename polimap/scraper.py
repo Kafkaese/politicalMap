4fc5ca6080f742601956d7d17b1fed780a85b959
Frederik Laubisch <kafkaese@gmail.com>
2022-11-13 09:34:00 +0100

    get info for first country per letter

--- polimap/scraper.py
+++ polimap/scraper.py
@@ -1,16 +1,31 @@
 import requests as re
 from bs4 import BeautifulSoup
 
-res = re.get('https://en.wikipedia.org/wiki/List_of_ruling_political_parties_by_country').content
 
-soup = BeautifulSoup(res, 'html.parser')
+def get_all_countries():
+    
 
-tables = soup.find_all('tbody')[6] 
+    res = re.get('https://en.wikipedia.org/wiki/List_of_ruling_political_parties_by_country').content
 
-rows = tables.find_all('tr')[1] # index 0 is header
+    soup = BeautifulSoup(res, 'html.parser')
 
-parties = rows.find_all('td')[2]
+    tables = soup.find_all('tbody')[6:] 
 
-ruling_party, link = parties.find('b').find('a').get('title'), parties.find('b').find('a')['href']
+    for table in tables:
+        rows = table.find_all('tr')[1] # index 0 is header
 
-print(ruling_party, link)
+        countries = rows.find_all('td')[0].find('a').text
+        parties = rows.find_all('td')[2]
+
+        party_info =parties.find('b')
+        #print(party_info)
+        
+        ruling_party = party_info.find('a').get('title', 'No Parties') if party_info != None else None
+        
+        # For non partisan political systems
+        link = party_info.find('a')['href'] if party_info != None else None
+        
+        #print('PARTY', ruling_party)
+        print(countries, ruling_party, link)
+
+get_all_countries()
