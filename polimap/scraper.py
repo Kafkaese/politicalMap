5a0ed926d72084f79250f657dd980a3957f2277f
Frederik Laubisch <kafkaese@gmail.com>
2022-11-13 09:11:51 +0100

    scrape one country

--- polimap/scraper.py
+++ polimap/scraper.py
@@ -5,4 +5,12 @@ res = re.get('https://en.wikipedia.org/wiki/List_of_ruling_political_parties_by_
 
 soup = BeautifulSoup(res, 'html.parser')
 
-print(soup)
+tables = soup.find_all('tbody')[6] 
+
+rows = tables.find_all('tr')[1] # index 0 is header
+
+parties = rows.find_all('td')[2]
+
+ruling_party, link = parties.find('b').find('a').get('title'), parties.find('b').find('a')['href']
+
+print(ruling_party, link)
